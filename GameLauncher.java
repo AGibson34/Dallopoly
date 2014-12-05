85b3f98d0564de242f4f7dbc9daa85fdaa530ae3
Andre Gibson <gibsonmiata@gmail.com>
2014-12-05 16:51:55 -0600

    GameLauncher.java
    This class handles the call for application startup.

---
+++ GameLauncher.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+
+/**
+ * This class will launch the Dallopoly game
+ * @author Andree Gibson  CS 2336  Assignment 6
+ *
+ */
+public class GameLauncher {
+	public static void main(String[] args) throws FileNotFoundException {
+		new DallopolyWindow();
+	}
+}
